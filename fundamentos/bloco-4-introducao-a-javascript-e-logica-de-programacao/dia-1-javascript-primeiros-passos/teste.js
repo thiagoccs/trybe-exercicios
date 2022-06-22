31a2d0ac6c120f398773b79b53a67adfa85ac4a8
Thiago Souza <thiago29.06@hotmail.com>
2022-06-22 19:02:01 -0300

    fundamentos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
@@ -0,0 +1,56 @@
+// let a = 15;
+// let b = 10;
+
+// let adição = (a + b);
+
+// console.log(adição);
+
+// let multiplicação = (a * b);
+
+// console.log(multiplicação);
+
+// let divisão = (a / b);
+
+// console.log(divisão);
+
+// let subtração = (a - b);
+
+// console.log(subtração);
+
+// let módulo = (a % b);
+
+// console.log(módulo);
+
+// let a = 15;
+// let b = 10;
+
+// switch(maior) {
+//     case 1: (a > b);
+//     console.log(maior);
+//     break;
+//     case 2: (a < b);
+//     console.log(maior);
+//     break;
+//     default: (a > b);
+//     console.log(maior);
+//     break;
+// }
+
+// const grade = 75; 
+// const nota = 50;
+// if (nota >= 80) { 
+//     console.log("Parabéns");
+// } else if (nota < 80 && nota <= 60) {
+//     console.log("Lista de Espera");
+// } else if (nota < 60) {
+//     console.log("Reprovado");
+// }
+
+// if (grade >= 80) {
+//     console.log("Parabéns, você foi aprovada(o)!")
+//   } else if (grade < 80 && grade >= 60) {
+//     console.log("Você está na nossa lista de espera");
+//   } else if (grade < 60) {
+//     console.log("Você foi reprovada(o)")
+//   }
+
