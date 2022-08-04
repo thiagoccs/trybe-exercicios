c9545ccadb288610b7f4136c9cd4b7a03073dba4
Thiago Souza <thiago29.06@hotmail.com>
2022-08-04 19:52:26 -0300

    4°

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -70,18 +70,35 @@
 // // 2°
 // setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
 
-// exercicio 3
+// // exercicio 3
 
-const getPlanet = () => {
-    const mars = {
-      name: "Mars",
-      distanceFromSun: {
-        value: 227900000,
-        measurementUnit: "kilometers",
-      },
-    };
-    console.log("Returned planet: ", mars);
-  };
+// const getPlanet = () => {
+//     const mars = {
+//       name: "Mars",
+//       distanceFromSun: {
+//         value: 227900000,
+//         measurementUnit: "kilometers",
+//       },
+//     };
+//     console.log("Returned planet: ", mars);
+//   };
   
-  setTimeout(() => getPlanet(), 4000); // imprime Marte depois de 4 segundos
+//   setTimeout(() => getPlanet(), 4000); // imprime Marte depois de 4 segundos
 
+
+// exercicio 4
+
+  const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = () => {
+    setTimeout (() => console.log(`A temperatura de Marte é: ${getMarsTemperature()}°C`), messageDelay());
+}; // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+sendMarsTemperature();
