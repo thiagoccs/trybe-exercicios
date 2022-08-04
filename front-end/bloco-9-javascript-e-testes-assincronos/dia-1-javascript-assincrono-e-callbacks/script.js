81cc4627611cd73ceae741a757a1eb2780b492f3
Thiago Souza <thiago29.06@hotmail.com>
2022-08-04 19:33:00 -0300

    2°

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -1,3 +1,42 @@
+//1° exercício
+
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+// const mars = {
+//   name: "Mars",
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const venus = {
+//   name: "Venus",
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const jupiter = {
+//   name: "Jupiter",
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// //1°
+// console.log(planetDistanceFromSun(mars)); // A 
+// //2°
+// console.log(planetDistanceFromSun(venus)); // B
+// //3°
+// console.log(planetDistanceFromSun(jupiter)); // C
+
+
+//2° exercício
+
 const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
   `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
@@ -24,10 +63,9 @@ const jupiter = {
     measurementUnit: "kilometers",
   },
 };
-
-//1°
-console.log(planetDistanceFromSun(mars)); // A 
-//2°
-console.log(planetDistanceFromSun(venus)); // B
-//3°
-console.log(planetDistanceFromSun(jupiter)); // C
+// 1°
+console.log(planetDistanceFromSun(mars)); // A
+// 3°
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+// 2°
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
